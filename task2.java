cc383a21949870f0da18d85dcd48a4b95daf9602
m0uZ <m.fraltsov@gmail.com>
2023-04-21 23:44:43 +0300

    result

---
+++ task2.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class task2 {
+    public static int[] elementDivideArray(int[] array1, int[] array2) {
+        if (array1.length != array2.length) {
+            throw new RuntimeException("Длинны массивов не равны!");
+        }
+        int[] result = new int[array1.length];
+        for (int i = 0; i < array1.length; i++) {
+            if (array2[i] == 0) {
+                throw new RuntimeException("Делить на 0 нельзя!");
+            }
+            result[i] = array1[i] / array2[i];
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] arr = {12,32,44,45};
+        int[] arr1 = {2,43,43,65};
+    
+        int[] res = elementDivideArray(arr, arr1);
+        System.out.println(Arrays.toString(res));
+    }
+}
