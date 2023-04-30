5df4bff594dcb54ae2551e153f31e589b025ee85
m0uZ <m.fraltsov@gmail.com>
2023-04-30 20:16:15 +0300

    ДЗ семинар 2 задача 1

---
+++ dzSeminar2Task1.java
@@ -0,0 +1,24 @@
+// Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float), 
+// и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения, 
+// вместо этого, необходимо повторно запросить у пользователя ввод данных.
+
+import java.util.Scanner;
+
+public class dzSeminar2Task1 {
+    public static float FloatInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите дробное число: ");
+        while (!scanner.hasNextFloat()) {
+            System.out.print("Некорректный ввод. Попробуйте еще: ");
+            scanner.next();
+        }
+        float input = scanner.nextFloat();
+        scanner.close();
+        return input;
+    }
+
+    public static void main(String[] args) {
+        float userInput = dzSeminar2Task1.FloatInput();
+        System.out.println("Введенное пользователем число: " + userInput);
+    }
+}
